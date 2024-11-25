d7402293d91125008d09b726846769a3ab774a4d
One Jack Lee <dev@wanjack.one>
2024-11-25 16:39:59 +0800

    Rename Tools.NumberToWordsConversion to Tools.NumberToWordsConversion.Web

--- Tools.NumberToWordsConversion.Web/Pages/Error.cshtml.cs
+++ Tools.NumberToWordsConversion.Web/Pages/Error.cshtml.cs
@@ -1,8 +1,9 @@
 using System.Diagnostics;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Tools.NumberToWordsConversion.Pages;
+namespace Tools.NumberToWordsConversion.Web.Pages;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
