6c3717fc4e18c4e734e2456eba6b2c8089cae65f
One Jack Lee <dev@wanjack.one>
2024-11-25 21:35:43 +0800

    Revert back to blazor template

--- Tools.NumberToWordsConversion.Web/Program.cs
+++ Tools.NumberToWordsConversion.Web/Program.cs
@@ -1,29 +1,28 @@
-using Tools.NumberToWordsConversion.Web.Extensions;
+using Tools.NumberToWordsConversion.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddApplication();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
 
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
+app.UseAntiforgery();
 
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
